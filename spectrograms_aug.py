8451dd3860ae583c8129c53c846dd400f2c9e9ac
Gianmarco Scarano <gianmarcoscarano@gmail.com>
2022-02-17 00:13:50 +0100

    Fix script di data augmentation - Aggiunte alcune print - Pulizia delle cartelle di data augmentation prima di iniziare (duplicated files) - Rimozione codice non più utile - Una volta generati i file, andiamo ad aggiornare i CSV relativi - Riordinamento CSV in base al nome dei files
    Signed-off-by: Gianmarco Scarano <gianmarcoscarano@gmail.com>

--- spectrograms_aug.py
+++ spectrograms_aug.py
@@ -2,120 +2,69 @@ import os
 import random
 import time
 import cv2
+import csv
 import IPython.display as ipd
 import librosa
 import librosa.display
 import numpy as np
-import pylab
 import soundfile as sf
+import shutil
 import pandas as pd
 
-
-from matplotlib import cm
 from tqdm import tqdm
 
-def timeshifter (y,sr):
-    start_ = int(np.random.uniform(-4800,4800))
-    print('time shift: ',start_)
-    if start_ >= 0:
-        wav_time_shift = np.r_[y[start_:], np.random.uniform(-0.001,0.001, start_)]
-    else:
-        wav_time_shift = np.r_[np.random.uniform(-0.001,0.001, -start_), y[:start_]]
-        ipd.Audio(wav_time_shift, rate=16000)
-        M = librosa.feature.melspectrogram(wav_time_shift, sr, 
-           fmax = sr/2, # Maximum frequency to be used on the on the MEL scale
-           n_fft=2048, 
-           hop_length=512, 
-                                                   n_mels = 96, # As per the Google Large-scale audio CNN paper
-                                                   power = 2) # Power = 2 refers to squared amplitude
-                
-                # Power in DB
-    log_power = librosa.power_to_db(M, ref=np.max)# Covert to dB (log) scale
-    pylab.figure(figsize=(3,3))
-    pylab.axis('off') 
-    pylab.axes([0., 0., 1., 1.], frameon=False, xticks=[], yticks=[]) # Remove the white edge
-    librosa.display.specshow(log_power, cmap=cm.jet)
-    #pylab.savefig(IMG_DIR + f[:-4]+'-1.jpg', bbox_inches=None, pad_inches=0)
-    #pylab.close()
-
-def speedchange(y,sr):
-    speed_rate = np.random.uniform(0.7,1.3)
-    wav_speed_tune = cv2.resize(y, (1, int(len(y) * speed_rate))).squeeze()
-    if len(wav_speed_tune) < y.shape[0]:
-        pad_len = y.shape[0] - len(wav_speed_tune) 
-        wav_speed_tune = np.r_[np.random.uniform(-0.001,0.001,int(pad_len/2)),
-                               wav_speed_tune,
-                               np.random.uniform(-0.001,0.001,int(np.ceil(pad_len/2)))]
-    else: 
-        cut_len = len(wav_speed_tune) - y.shape[0]
-        wav_speed_tune = wav_speed_tune[int(cut_len/2):int(cut_len/2)+y.shape[0]]
-    print('wav length: ', wav_speed_tune.shape[0])
-    ipd.Audio(wav_speed_tune, rate=sr)
-    M = librosa.feature.melspectrogram(wav_speed_tune, sr, 
-       fmax = sr/2, # Maximum frequency to be used on the on the MEL scale
-       n_fft=2048, 
-       hop_length=512, 
-                                               n_mels = 96, # As per the Google Large-scale audio CNN paper
-                                               power = 2) # Power = 2 refers to squared amplitude
-            
-            # Power in DB
-    log_power = librosa.power_to_db(M, ref=np.max)# Covert to dB (log) scale
-    pylab.figure(figsize=(3,3))
-    pylab.axis('off') 
-    pylab.axes([0., 0., 1., 1.], frameon=False, xticks=[], yticks=[]) # Remove the white edge
-    librosa.display.specshow(log_power, cmap=cm.jet)
-    #pylab.savefig(IMG_DIR + f[:-4]+'-2.jpg', bbox_inches=None, pad_inches=0)
-    #pylab.close()
-
-def noiseback(y,sr):
-    bg_files = os.listdir('D:/Users/PC/Downloads/15-Free-Ambient-Sound-Effects/15 Free Ambient Sound Effects')
-    chosen_bg_file = bg_files[np.random.randint(6)]
-    bg, sr = librosa.load('D:/Users/PC/Downloads/15-Free-Ambient-Sound-Effects/15 Free Ambient Sound Effects/'+chosen_bg_file, sr=16000)
-    print(chosen_bg_file,'|', bg.shape[0], bg.max(), bg.min())
-    ipd.Audio(bg, rate=sr) # !! be prepared when playing the noise, bacause it's so ANNOYING !!
-    start_ = np.random.randint(bg.shape[0]-y.shape[0])
-    bg_slice = bg[start_ : start_+ y.shape[0]]
-    wav_with_bg = y * np.random.uniform(0.8, 1.2) + bg_slice * np.random.uniform(0, 0.1)
-    ipd.Audio(wav_with_bg, rate=sr) 
+def getValues(nomeFile):
+    emozione = ""
+    valenza = ""
+    arousal = ""
     
-    M = librosa.feature.melspectrogram(wav_with_bg, sr, 
-       fmax = sr/2, # Maximum frequency to be used on the on the MEL scale
-       n_fft=2048, 
-       hop_length=512, 
-                                               n_mels = 96, # As per the Google Large-scale audio CNN paper
-                                               power = 2) # Power = 2 refers to squared amplitude
-            
-            # Power in DB
-    log_power = librosa.power_to_db(M, ref=np.max)# Covert to dB (log) scale
-    pylab.figure(figsize=(3,3))
-    pylab.axis('off') 
-    pylab.axes([0., 0., 1., 1.], frameon=False, xticks=[], yticks=[]) # Remove the white edge
-    librosa.display.specshow(log_power, cmap=cm.jet)
-    #pylab.savefig(IMG_DIR + f[:-4]+'-3.jpg', bbox_inches=None, pad_inches=0)
-    #pylab.close()
+    if (nomeFile == 'dis'):
+        emozione = "Disgusto"
+        valenza = "Negativa"
+        arousal = "Bassa" 
+    elif (nomeFile == 'gio') :
+        emozione = "Gioia"
+        valenza = "Positiva"
+        arousal = "Alta"
+    elif (nomeFile == 'pau'):
+        emozione = "Paura"
+        valenza = "Neutrale"
+        arousal = "Alta"
+    elif (nomeFile == 'rab'):
+        emozione = "Rabbia"
+        valenza = "Negativa"
+        arousal = "Alta"
+    elif (nomeFile == 'sor'):
+        emozione = "Sorpresa"
+        valenza = "Positiva"
+        arousal = "Media"
+    elif (nomeFile == 'tri'):
+        emozione = "Tristezza"
+        valenza = "Negativa"
+        arousal = "Bassa"
+    elif (nomeFile == 'col'):
+        emozione = "Colpa"
+        valenza = "Negativa"
+        arousal = "Bassa"
+    else :
+       emozione = "Neutrale"
+       valenza = "Neutrale"
+       arousal = "Media"
+    return emozione, valenza, arousal
 
 def alltransformations(y, sr, fileName, i, training_dir, dataFrame):
-    # time shift
+
+    # ------ Time Shift------ #
     start_ = int(np.random.uniform(-4800,4800))
-    #print('time shift: ',start_)
     if start_ >= 0:
         wav_time_shift = np.r_[y[start_:], np.random.uniform(-0.001,0.001, start_)]
     else:
         wav_time_shift = np.r_[np.random.uniform(-0.001,0.001, -start_), y[:start_]]
     ipd.Audio(wav_time_shift, rate=16000)
                  
-    #aumento velocità
+    # ------ Aumento velocità ------ #
     speed_rate = np.random.uniform(0.7,1.3)
     wav_speed_tune = cv2.resize(wav_time_shift, (1, int(len(wav_time_shift) * speed_rate))).squeeze()
-    #print('speed rate: %.3f' % speed_rate, '(lower is faster)')
-    '''if len(wav_speed_tune) < 16000:
-        pad_len = len(wav_speed_tune) 
-        wav_speed_tune = np.r_[np.random.uniform(-0.001,0.001,int(pad_len/2)),
-                               wav_speed_tune,
-                               np.random.uniform(-0.001,0.001,int(np.ceil(pad_len/2)))]
-    else: 
-        cut_len = len(wav_speed_tune) - 16000
-        wav_speed_tune = wav_speed_tune[int(cut_len/2):int(cut_len/2)+16000]'''
     if len(wav_speed_tune) < wav_time_shift.shape[0]:
         pad_len = wav_time_shift.shape[0] - len(wav_speed_tune) 
         wav_speed_tune = np.r_[np.random.uniform(-0.001,0.001,int(pad_len/2)),
@@ -124,19 +73,15 @@ def alltransformations(y, sr, fileName, i, training_dir, dataFrame):
     else: 
         cut_len = len(wav_speed_tune) - wav_time_shift.shape[0]
         wav_speed_tune = wav_speed_tune[int(cut_len/2):int(cut_len/2)+wav_time_shift.shape[0]]
-    #print('wav length: ', wav_speed_tune.shape[0])
     ipd.Audio(wav_speed_tune, rate=sr)
     
-    #rumori di sottofondo
+    # ------ Rumori di sottofondo ------ #
     randomBG = np.random.randint(len(dataFrame))
     bg = dataFrame.at[randomBG,'bg']
     sr = dataFrame.at[randomBG,'sr']
-    #print(chosen_bg_file,'|', bg.shape[0], bg.max(), bg.min())
     ipd.Audio(bg, rate=sr) # !! be prepared when playing the noise, bacause it's so ANNOYING !!
     start_ = np.random.randint(bg.shape[0]-y.shape[0])
     bg_slice = bg[start_ : start_+ wav_speed_tune.shape[0]]
-    
-    #wav_with_bg = wav_speed_tune * np.random.uniform(0.8, 1.2) + bg_slice * np.random.uniform(0, 0.1)
     
     wav_with_bg = bg_slice * np.random.uniform(0, 0.1)
     ipd.Audio(wav_with_bg, rate=sr) 
@@ -147,21 +92,129 @@ def alltransformations(y, sr, fileName, i, training_dir, dataFrame):
     
     # Ci salviamo l'audio modificato
     sf.write(os.path.join(training_dir, newName).replace('\\','/'), finalArray, sr)
-    
-    #M = librosa.feature.melspectrogram(wav_with_bg, sr, 
-    #   fmax = sr/2, # Maximum frequency to be used on the on the MEL scale
-    #   n_fft=2048, 
-    #   hop_length=512, 
-    #   n_mels = 96, # As per the Google Large-scale audio CNN paper
-    #   power = 2) # Power = 2 refers to squared amplitude
+        
+    if(training_dir.split("/")[1] == 'emovo'):
+        nomeFile = fileName.split("/")[2][0:3]
+        
+        genereFile = fileName.split("/")[2].split("-")[1]
+        
+        if(genereFile.startswith("f")):
+            genere = 'Donna'
+            genereCartella = "train_emovo_female"
+            gender = 'female'
+        else:
+            genere = 'Uomo'
+            genereCartella = "train_emovo_male"
+            gender = 'male'
+        
+        emozione, valenza, arousal = getValues(nomeFile)
             
-    # Power in DB
-    #log_power = librosa.power_to_db(M, ref=np.max)# Covert to dB (log) scale
-    #return log_power
+        riga = ['/{}/'.format(training_dir.split("/")[2]) + newName, emozione, valenza, arousal, genere]           
+        
+        with open('{}/{}.csv'.format(datasetsDirectory,genereCartella), 'a', encoding='UTF8',newline='') as f:
+            writer = csv.writer(f,delimiter=';', quotechar='|', 
+                                quoting=csv.QUOTE_MINIMAL, 
+                                lineterminator="\n")
+            # scriviamo la riga
+            writer.writerow(riga)
+            f.close()
+
+        with open('{}/train_emovo_all.csv'.format(datasetsDirectory), 'a', encoding='UTF8',newline='') as f:
+            writer = csv.writer(f,delimiter=';', quotechar='|', 
+                                quoting=csv.QUOTE_MINIMAL, 
+                                lineterminator="\n")
+            # scriviamo la riga
+            writer.writerow(riga)
+            f.close()
+           
+        with open('{}/all_train_{}.csv'.format(datasetsDirectory, gender), 'a', encoding='UTF8',newline='') as f:
+            writer = csv.writer(f,delimiter=';',quotechar='|', quoting=csv.QUOTE_MINIMAL, lineterminator="\n")
+                      
+            # scriviamo la riga
+            writer.writerow(riga)  
+            f.close()
+            
+        with open('{}/all_train.csv'.format(datasetsDirectory), 'a', encoding='UTF8',newline='') as f:
+            writer = csv.writer(f,delimiter=';',quotechar='|', quoting=csv.QUOTE_MINIMAL, lineterminator="\n")
+                      
+            # scriviamo la riga
+            writer.writerow(riga)
+            f.close()
+    else:
+        if(fileName.split("/")[2].startswith("NP") or fileName.split("/")[2].startswith("PR")):
+            voceAttore = fileName.split("/")[2].split("_")[1] 
+            nomeFile = fileName.split("/")[2][8:11]
+        else:
+            voceAttore = fileName.split("/")[2].split("_")[0]
+            nomeFile = fileName.split("/")[2][5:8]
+        
+        if(voceAttore == 'f'):
+            genere = 'Donna'
+            gender = 'female'
+            genereCartella = "train_demos_female"
+        else:
+            genere = 'Uomo'
+            gender = 'male'
+            genereCartella = "train_demos_male"
+        
+        emozione, valenza, arousal = getValues(nomeFile)
+                           
+        header = ['/{}/'.format(training_dir.split("/")[2]) + newName, emozione, valenza, arousal, genere]
+                   
+        with open('{}/{}.csv'.format(datasetsDirectory, genereCartella), 'a', encoding='UTF8',newline='') as f:
+            writer = csv.writer(f,delimiter=';',quotechar='|', quoting=csv.QUOTE_MINIMAL, lineterminator="\n")
+                      
+            # scriviamo la riga
+            writer.writerow(header)  
+            f.close()
+
+        with open('{}/train_demos_all.csv'.format(datasetsDirectory), 'a', encoding='UTF8',newline='') as f:
+            writer = csv.writer(f,delimiter=';',quotechar='|', quoting=csv.QUOTE_MINIMAL, lineterminator="\n")
+                      
+            # scriviamo la riga
+            writer.writerow(header)  
+            f.close()
+
+        with open('{}/all_train_{}.csv'.format(datasetsDirectory, gender), 'a', encoding='UTF8',newline='') as f:
+            writer = csv.writer(f,delimiter=';',quotechar='|', quoting=csv.QUOTE_MINIMAL, lineterminator="\n")
+                      
+            # scriviamo la riga
+            writer.writerow(header)  
+            f.close()
+            
+        with open('{}/all_train.csv'.format(datasetsDirectory), 'a', encoding='UTF8',newline='') as f:
+            writer = csv.writer(f,delimiter=';',quotechar='|', quoting=csv.QUOTE_MINIMAL, lineterminator="\n")
+                      
+            # scriviamo la riga
+            writer.writerow(header)  
+            f.close()
+    
     return
 
+def sortCSV():
+    
+    folders = os.listdir(datasetsDirectory)
+    
+    folders = list(filter(lambda x: x.endswith(".csv"), folders))
+    
+    print('-------------------------')
+    print("Ordino i vari CSV ...")
+    
+    for csvFile in folders:
+        
+        csvData = pd.read_csv("{}/{}".format(datasetsDirectory, csvFile), sep=';')
+        
+        csvData = csvData.reset_index(drop=True)
+        idx = csvData['NOME_FILE'].str.split('/', expand=True).sort_values([2,1,0]).index
+        csvData = csvData.reindex(idx).reset_index(drop=True)
+        
+        #csvData.sort_values(csvData.columns[0], 
+        #                axis=0,
+        #                inplace=True)
+        
+        csvData.to_csv("{}/{}".format(datasetsDirectory, csvFile), index=False, sep=";")
+
 def computeTransformation(wav_files):
-    log_power = []
 
     # Pre-load ambient sounds
     print('-------------------------')    
@@ -178,10 +231,11 @@ def computeTransformation(wav_files):
 
     print('-------------------------')
     print("Creo i file di data augmentation..")
+    
     wav_files.sort()
+    
     for f in tqdm(wav_files):
         try:
-            
             if(f.split("/")[1] == 'DEMOS' or f.split("/")[1] == 'NEU'):
                 dataSet = 'wav_DEMoS'
             else:
@@ -199,61 +253,29 @@ def computeTransformation(wav_files):
             pre_emphasis = 0.97
             y = np.append(y[0], y[1:] - pre_emphasis * y[:-1])
             
-            # Compute spectrogram
-            #M = librosa.feature.melspectrogram(y, sr, 
-                                               #fmax = sr/2, # Maximum frequency to be used on the on the MEL scale
-                                               #n_fft=2048, 
-                                               #hop_length=512, 
-                                               #n_mels = 96, # As per the Google Large-scale audio CNN paper
-                                               #power = 2) # Power = 2 refers to squared amplitude
-            
-            # Power in DB
-            #log_power = librosa.power_to_db(M, ref=np.max)# Covert to dB (log) scale
-            
-            # Plotting the spectrogram and save as JPG without axes (just the image)
-            #pylab.figure(figsize=(3,3))
-            #pylab.axis('off') 
-            #pylab.axes([0., 0., 1., 1.], frameon=False, xticks=[], yticks=[]) # Remove the white edge
-            #pylab.axes([0., 0., 1., 1.], frameon=False, xticks=[], yticks=[]) # Remove the white edge
-            #librosa.display.specshow(log_power, cmap=cm.jet)
-            #pylab.savefig(IMG_DIR + f[:-4]+'.jpg', bbox_inches=None, pad_inches=0)
-            #pylab.close()
-            
-            #aumento dati in mase a cambiamenti
-            '''timeshifter(y, sr)
-            speedchange(y, sr)
-            noiseback(y, sr)'''
-            
             i = 0
             
             # Implementa le varie trasformazioni per due volte
             # Ogni volta però, facciamo trasformazioni random
-            log_power = alltransformations(y, sr, f, i, training_dir, df)
-            #pylab.figure(figsize=(3,3))
-            #pylab.axis('off') 
-            #pylab.axes([0., 0., 1., 1.], frameon=False, xticks=[], yticks=[]) # Remove the white edge
-            #librosa.display.specshow(log_power, cmap=cm.jet)
-            #pylab.savefig(IMG_DIR + f[:-4] +'-1.jpg', bbox_inches=None, pad_inches=0)
-            #pylab.close()
+            alltransformations(y, sr, f, i, training_dir, df)
 
             i += 1
 
             # Impostiamo un random seed ogni volta
             random.seed(time.process_time())
             
-            log_power = alltransformations(y, sr, f, i, training_dir, df)
-            #pylab.figure(figsize=(3,3))
-            #pylab.axis('off') 
-            #pylab.axes([0., 0., 1., 1.], frameon=False, xticks=[], yticks=[]) # Remove the white edge
-            #librosa.display.specshow(log_power, cmap=cm.jet)
-            #pylab.savefig(IMG_DIR + f[:-4] +'-2.jpg', bbox_inches=None, pad_inches=0)
-            #pylab.close()
+            alltransformations(y, sr, f, i, training_dir, df)
             
         except Exception as e:
             print(f, e)
             pass
+        
+
 
 # ---------------------- MAIN ---------------------- #
+
+emovo = []
+WAVDemos = []
 
 datasetsDirectory = 'Datasets/'
 
@@ -261,29 +283,34 @@ datasetsDirectory = 'Datasets/'
 if not os.path.exists(datasetsDirectory):
     print("La cartella relativa ai Datasets non esiste!")
     print("Creare la cartella ''Datasets'' e inserirci i dataset necessari.")
+    print("- Eseguire ''python downloadDatasets.py''")
     exit(0)
 
 folders = os.listdir(datasetsDirectory)
 
-folders = list(filter(lambda x: not x.endswith(".csv") and not x == '15 Free Ambient Sound Effects', folders))
+folders = list(filter(lambda x: not x.endswith(".csv") and not x == '15 Free Ambient Sound Effects' and not x.endswith("tmp"), folders))
 
+print("Pulisco le cartelle di data augmentation ...")
 for folder in folders:
     
     dataAugmentation = "{}_augmentation".format(folder)
     
-    if not(os.path.exists(os.path.join(datasetsDirectory,folder,dataAugmentation))):
-           os.makedirs(os.path.join(datasetsDirectory,folder,dataAugmentation))
-    
+    if os.path.exists(os.path.join(datasetsDirectory,folder,dataAugmentation)):
+        shutil.rmtree((os.path.join(datasetsDirectory,folder,dataAugmentation)))
+        
+    os.makedirs(os.path.join(datasetsDirectory,folder,dataAugmentation))
+
 columns = ['NOME_FILE', 'EMOZIONE', 'VALENZA', 'AROUSAL', 'GENERE']
 df = pd.read_csv('{}/all_train.csv'.format(datasetsDirectory), sep=";", usecols=columns)
 wav_files = df['NOME_FILE'].tolist() #you can also use df['column_name']
 
 if(len(wav_files) > 0):
-    
+    sortCSV()    
     print('-------------------------')
     print("Trasformo le cartelle: {}, {}".format(folders[0], folders[1]))
     computeTransformation(wav_files)
-    print('\n')
+    print('-------------------------')
+    print("Data augmentation completata!")
 else:
     print("Sei sicuro che esista un CSV di training in: {datasetsDirectory}?")
     
